be2b2904135aa5be09469d95f2967c9f72c8184d
Sam Kester <samkester@gmail.com>
2020-10-02 09:46:41 -0600

    Coin model

---
+++ src/main/java/lambda/work/piggybank/models/CoinModel.java
@@ -0,0 +1,74 @@
+package lambda.work.piggybank.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coins")
+public class CoinModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long coinid;
+    private String name;
+    private String nameplural;
+    private double value;
+    private int quantity;
+
+    public CoinModel(String name, String nameplural, double value, int quantity) {
+        this.name = name;
+        this.nameplural = nameplural;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public CoinModel() {
+    }
+
+    public long getCoinid() {
+        return coinid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameplural() {
+        return nameplural;
+    }
+
+    public void setNameplural(String nameplural) {
+        this.nameplural = nameplural;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getValueTotal() {
+        return value * quantity;
+    }
+
+    @Override
+    public String toString() {
+        if(quantity == 1)
+        {
+            return quantity + " " + name;
+        }
+        return quantity + " " + nameplural;
+    }
+}
